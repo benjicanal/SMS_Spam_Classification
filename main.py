2b62662946f17e7b989ccdcd07052d72297585f4
benjamin <benjamin.faresin@umontreal.ca>
2024-08-29 16:55:01 +0200

    Finalize API Rest on FastAPI

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ from pydantic import BaseModel
 from fastapi import FastAPI
 import joblib
 import numpy as np
+from app import transform_text
 
 model = joblib.load('model.pkl')
 vectorizer = joblib.load('vectorizer.pkl')
 
 app = FastAPI()
 
-@app.get('/')
-async def index():
-    return {'message': 'Welcome to the Spam Classifier API!'}
-
 class SpamData(BaseModel) : 
     text : str
     
 @app.post('/predict')
 async def predict(data : SpamData) : 
-    return {'prediction' : prediction}
+    try : 
+        transf_text = transform_text(data.text) 
+        vector_input = vectorizer.transform([transf_text])
+        result = model.predict(vector_input)[0]
+        result = int(result)
+        return {'prediction' : result}
+    except Exception as e : 
+        return {'error' : str(e)}
