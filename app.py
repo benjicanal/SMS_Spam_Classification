749d42fa0e5394fbeef13dc93fde709fa95ea8b8
benjamin <benjamin.faresin@umontreal.ca>
2024-08-31 23:42:58 +0200

    App on streamlit

--- app.py
+++ app.py
@@ -1,42 +1,9 @@
 import streamlit as st
 import joblib
 from utils import transform_text
-# from nltk.corpus import stopwords
-# import string
-# from nltk.stem import PorterStemmer
-# import nltk
-
-# nltk.download('punkt')
-# nltk.download('stopwords')
-
 
 tfid = joblib.load(open('vectorizer.pkl', 'rb'))
 model = joblib.load(open('model.pkl', 'rb'))
-
-
-# def transform_text(text):
-#     text = text.lower()
-#     text = nltk.word_tokenize(text)
-
-#     y = []
-#     for i in text:
-#         if i.isalnum():
-#             y.append(i)
-
-#     text = y[:]
-#     y.clear()
-
-#     for i in text:
-#         if i not in stopwords.words('english') and i not in string.punctuation:
-#             y.append(i)
-
-#     text = y[:]
-#     y.clear()
-#     ps = PorterStemmer()
-#     for i in text:
-#         y.append(ps.stem(i))
-#     return " ".join(y)
-
 
 input_sms = st.text_input("Enter the SMS/Email")
 st.title("Email/SMS Spam Classifier")
